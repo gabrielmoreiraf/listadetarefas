f237b07d210f8000fbf97a8273e90942a7e1c7cc
gabriel <gabrielfmoreira1@gmail.com>
2025-11-05 17:50:15 -0300

    quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import {useState}  from 'react';
+import Todo from './components/Todo';
 import "./App.css"
+import TodoForm from './components/TodoForm';
 
 
 
 function App() {
-  const [todos, setTodos] = useState([
+  const [todos] = useState([
     {
       id: 1,
-      text: "Criar funcionalidade X no sistema",
-      category:"Trabalh0",
+      text: "Criar funcionalidade no sistema",
+      category:"Trabalho",
       isCompleted: false,
     },
     {
@@ -29,20 +31,11 @@ function App() {
     <h1>Lista de Tarefas</h1>
     <div className='todo-list'>
       {todos.map((todo) => (
-        <div className="todo">
-          <div className="content">
-          <p>{todo.text}</p>
-          <p className="category">
-          ({todo.category})
-          </p>
-          </div>
-          <div>
-            <button>Completar</button>
-            <button>X</button>
-          </div>
-        </div>
+    <Todo key={todo.id} todo={todo} />
       ))}
+
     </div>
+    <TodoForm />
   </div>
 }
 
